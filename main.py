13f1ef0167c18fe955099a792a94ef1eb96cad07
Ethan Robinson <ethanrobinson1197@gmail.com>
2023-11-17 17:15:25 -0800

    Main.py processes data from arguments.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # Created by Ethan Robinson
 # Started November 17th, 2023
 
-
+import sys
+from services import password_processor
 
 if __name__ == "__main__":
-    ...
+    # TODO: add error checking 
+    
+    password = sys.argv[1]
+    image_file_path = sys.argv[2]
+    preprocessed_pwd = password_processor.password_processor(password)
