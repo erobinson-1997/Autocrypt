9835c5acf821ed57e23fd3f53b88e6c8b22b660e
Ethan Robinson <ethanrobinson1197@gmail.com>
2023-12-22 13:22:23 -0800

    commiting everything

--- models/ImageDecoder.py
+++ models/ImageDecoder.py
@@ -10,13 +10,15 @@ class ImageDecoder(Model):
     def __init__(self, in_val):
         super(ImageDecoder, self).__init__()
         self._input_value = in_val 
-        self._input_length = in_val.shape[0]
+        self._input_length = in_val.shape[1]
 
     def _dencoder(self):
         input_layer = layers.Input(shape=(self._input_length, 3))
         
         decoder = tf.keras.Sequential([
             input_layer,
+            layers.LSTM(300),
+            layers.Reshape((10, 10, 3)),  # Reshape to a more 'image-like' shape
             layers.Conv2DTranspose(32, (2, 2), activation='relu', padding='same'),
             layers.UpSampling2D((4, 4)),
             layers.Conv2DTranspose(64, (2, 2), activation='relu', padding='same'),
